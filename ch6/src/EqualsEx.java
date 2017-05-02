b1b8677bb6dc9dbb5a3ae61533d64f15c9544265
zamar8905 <zamar8905@naver.com>
2017-05-03 00:20:36 +0900

    Ex1

---
+++ ch6/src/EqualsEx.java
@@ -0,0 +1,11 @@
+
+public class EqualsEx {
+	public static void main(String[] args) {
+		Rect a = new Rect(2,3);
+		Rect b = new Rect(6,1);
+		Rect c = new Rect(5,7);
+		if(a.equals(b)) System.out.println("a is equal to b");
+		if(a.equals(c)) System.out.println("a is equal to c");
+		if(b.equals(c)) System.out.println("b is equal to c");
+	}
+}
