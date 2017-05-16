2d689b78c58e4afb378b61c51f9eafb89c60b43a
zamar8905 <zamar8905@naver.com>
2017-05-16 23:31:42 +0900

    ex5

---
+++ ch6/src/ex5/StringBufferEx.java
@@ -0,0 +1,21 @@
+package ex5;
+
+public class StringBufferEx {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This");
+		System.out.println(sb.hashCode());
+		sb.append(" is pencil");
+		System.out.println(sb);
+		
+		sb.insert(7, " my");
+		System.out.println(sb);
+
+		sb.replace(8, 10, "your");
+		System.out.println(sb);
+
+		sb.setLength(5);
+		
+		System.out.println(sb);
+		System.out.println(sb.hashCode());
+	}
+}
